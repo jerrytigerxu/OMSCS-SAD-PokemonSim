2a882223aa95cfea7e4ab776ffe739ee5e284a89
sshaikh65 <sshaikh65@gatech.edu>
2024-11-23 20:51:29 -0500

    working on Eevee and Psyduck

---
+++ backend/src/main/java/cs6310/Pokemon/Eevee.java
@@ -0,0 +1,19 @@
+package cs6310.Pokemon;
+
+import cs6310.Pokemon.dto.Pokemon;
+
+public class Eevee extends Pokemon {
+
+    public Eevee() {
+        super();
+    }
+
+    public Eevee(int seed) {
+        super(seed);
+        setStats();
+    }
+
+    private void setStats() {
+        
+    }
+}
---
+++ backend/src/main/java/cs6310/Pokemon/Psyduck.java
@@ -0,0 +1,20 @@
+package cs6310.Pokemon;
+
+import cs6310.Pokemon.dto.Pokemon;
+
+public class Psyduck extends Pokemon{
+    
+    public Psyduck() {
+        super();
+    }
+
+    public Psyduck(int seed) {
+        super(seed);
+        setStats();
+    }
+        
+    private void setStats() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setStats'");
+    }
+}
