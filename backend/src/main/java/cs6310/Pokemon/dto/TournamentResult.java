ac57d524e709a233b2644833fbdee45960044ec2
Jere Xu <jxu372@gatech.edu>
2024-11-27 12:37:01 -0500

    Adjusted the backend logic regarding tournaments so that the API call from the frontend works properly

--- backend/src/main/java/cs6310/Pokemon/dto/TournamentResult.java
+++ backend/src/main/java/cs6310/Pokemon/dto/TournamentResult.java
@@ -1,6 +1,8 @@
 package cs6310.Pokemon.dto;
 
 import java.util.List;
+import java.util.ArrayList;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,9 +10,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
+
 import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 
 @Data
 @Entity
@@ -25,5 +29,13 @@ public class TournamentResult {
 
     @OneToMany
     @JsonProperty("battleResultList")
-    private List<BattleResult> battleResultList;
+    private List<BattleResult> battleResultList = new ArrayList<>();
+
+    public List<BattleResult> getBattleResultList() {
+        return battleResultList;
+    }
+
+    public void setWinnerPokemon(String winnerPokemon) {
+        this.winnerPokemon = winnerPokemon;
+    }
 }
