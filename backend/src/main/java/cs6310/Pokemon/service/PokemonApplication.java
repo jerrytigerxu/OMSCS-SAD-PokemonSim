722429ec5906df65f9a68329c6069850b19b5a24
Sam Hrncir <samhrncir@gmail.com>
2024-11-26 22:35:48 -0500

    update deps

--- backend/src/main/java/cs6310/Pokemon/service/PokemonApplication.java
+++ backend/src/main/java/cs6310/Pokemon/service/PokemonApplication.java
@@ -2,10 +2,12 @@ package cs6310.Pokemon.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication(scanBasePackages = { "cs6310.Pokemon.service", "cs6310.Pokemon.dto",
 		"cs6310.Pokemon.controller" })
+@EnableJpaRepositories(basePackages = "cs6310.Pokemon.repository")
 @EnableRetry
 public class PokemonApplication {
 
