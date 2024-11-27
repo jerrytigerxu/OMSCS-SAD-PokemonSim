9ee0f567280373c7e61badc03978850328e41c92
Sam Hrncir <samhrncir@gmail.com>
2024-11-26 22:45:23 -0500

    dynamically list all available pokemon

--- backend/src/main/java/cs6310/Pokemon/service/CommandService.java
+++ backend/src/main/java/cs6310/Pokemon/service/CommandService.java
@@ -10,12 +10,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.retry.annotation.Retryable;
 
 import cs6310.Pokemon.dto.BattleResult;
-import cs6310.Pokemon.exceptions.InvalidSeedException;
-import cs6310.Pokemon.exceptions.InvalidSeedException;
+import cs6310.Pokemon.dto.TournamentResult;
+import cs6310.Pokemon.exception.InvalidSeedException;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import java.io.File;
 import java.sql.SQLException;
+import org.reflections.Reflections;
+import cs6310.Pokemon.dto.Pokemon;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,9 +36,7 @@ public class CommandService {
         System.out.println("> " + inputString);
 
         if (tokens[0].startsWith("//")) {
-            // for (var i = 0; i < tokens.length; i++) {
-            // System.out.println("token " + i + " is " + tokens[i]);
-            // }
+            // do nothing
         } else if (tokens[0].equals("setseed")) {
             doSetSeed(Integer.parseInt(tokens[1]));
         } else if (tokens[0].equals("removeseed")) {
@@ -125,12 +126,12 @@ public class CommandService {
     }
 
     @Retryable(value = { SQLException.class }, maxAttempts = 3)
-    public BattleResult doTournament(List<String> pokemonList) throws InvalidSeedException {
+    public TournamentResult doTournament(List<String> pokemonList) throws InvalidSeedException {
         if (this.seed < 0) {
             throw new InvalidSeedException();
         }
-        // TODO: implement this method
-        return new BattleResult();
+        Tournament tournament = new Tournament(this.seed, pokemonList);
+        return tournament.startTournament();
     }
 
     @Retryable(value = { SQLException.class }, maxAttempts = 3)
@@ -154,7 +155,12 @@ public class CommandService {
         System.out.println("Welcome to the thunder dome!");
     }
 
-    public static List<String> getAllPokemon() {
-        return List.of("Charmander", "Pikachu", "Squirtle", "Bulbasaur");
+    public List<String> getAllPokemon() {
+        Reflections reflections = new Reflections("cs6310.Pokemon");
+        Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
+        return allClasses.stream()
+                .filter(cls -> cls.getPackage().getName().equals("cs6310.Pokemon"))
+                .map(Class::getSimpleName)
+                .collect(Collectors.toList());
     }
 }
