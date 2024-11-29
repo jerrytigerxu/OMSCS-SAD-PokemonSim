6ecf45096c68ea7dc4f863294daa5efe15842b6f
umilojkovic <uroshm@gmail.com>
2024-11-29 17:43:24 -0500

    include missing import for repository annotation.

--- backend/src/main/java/cs6310/Pokemon/repository/BattleResultRepository.java
+++ backend/src/main/java/cs6310/Pokemon/repository/BattleResultRepository.java
@@ -1,6 +1,8 @@
 package cs6310.Pokemon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import cs6310.Pokemon.model.entity.BattleResultEntity;
 import java.time.LocalDateTime;
 
